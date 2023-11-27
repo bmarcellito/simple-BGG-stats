fba2292343f625378feed75dfd801a1d60e8d552
bmarcellito <150475348+bmarcellito@users.noreply.github.com>
2023-11-27 19:43:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,22 @@ import re
 @st.cache_data
 def gdrive_authenticate() -> googleapiclient.discovery.Resource:
     scopes = ['https://www.googleapis.com/auth/drive']
-    service_account_file = 'service_account.json'
-    creds = service_account.Credentials.from_service_account_file(service_account_file, scopes=scopes)
+
+    service_account_info = {
+      "type": "service_account",
+      "project_id": "simple-bgg-stat-service-acc",
+      "private_key_id": st.secrets["private_key_id"],
+      "private_key": st.secrets["private_key"],
+      "client_email": st.secrets["client_email"],
+      "client_id": st.secrets["client_id"],
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/simple-bgg-stat-sa%40simple-bgg-stat-service-acc.iam.gserviceaccount.com",
+      "universe_domain": "googleapis.com"
+    }
+    
+    creds = service_account.Credentials.from_service_account_info(service_account_info, scopes=scopes)
     service = build('drive', 'v3', credentials=creds)
     return service
 
